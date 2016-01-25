e824b7a0dfdb4ae5cf0adb6e8333b7d00f77d85c
suhussai <suhussai@ualberta.ca>
2016-01-25 11:24:13 -0700

    app skeleton

---
+++ app/src/main/java/com/example/suhussai/as1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.suhussai.as1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+    }
+
+}
